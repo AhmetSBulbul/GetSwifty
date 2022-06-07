35a3420452bc66ec28e5220f4a4aa30624d1a4d0
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-06-07 19:36:14 +0300

    Initial Commit

---
+++ GetSwifty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetSwifty
+//
+//  Created by Ahmet Bülbül on 7.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetSwifty/GetSwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetSwiftyApp.swift
+//  GetSwifty
+//
+//  Created by Ahmet Bülbül on 7.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GetSwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
